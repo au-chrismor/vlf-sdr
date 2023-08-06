52d4ebb303e2ee266a78ba9f03aada662b282ee6
Christopher Moran <chris.moran@emergent.com.au>
2023-08-06 10:34:08 +1000

    Debugger updates

--- vlf-sdr/vlf-sdr.ino
+++ vlf-sdr/vlf-sdr.ino
@@ -12,66 +12,131 @@
 ***************************************/
 
 #include "vlfsdr.h"
+#include <Encoder.h>
 #include <LiquidCrystal_I2C.h>
+#include "thermistor.h"
 
+
+long oldValue  = 0;
+long frequency = FREQ_MIN;
+uint16_t temperature;
+
+Encoder vfoDial(ENC_CW, ENC_ACW);
 LiquidCrystal_I2C lcd(LCD_ADDR, LCD_COLS, LCD_ROWS);
+THERMISTOR thermistor(THERM, THERM_RES, THERM_BETA, THERM_SERIES);
 
-
+/*
+ * Standard Arduino Statup
+ */
 void setup() {
   lcd.init();
+  lcd.clear();
   lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.print("Please Wait...");
 #ifdef _DEBUG
   Serial.begin(9600);
-  Serial.println("Starting up");
+  Serial.println("Hardware Setup");
 #endif
+  lcd.setCursor(0, 0);
+  lcd.print("Startup Ver:");
+  lcd.setCursor(13, 0);
+  lcd.print(SW_VERSION);
+  
+  // configure arduino data pins for output  
   pinMode(FQ_UD, OUTPUT);
   pinMode(W_CLK, OUTPUT);
   pinMode(DATA, OUTPUT);
   pinMode(RESET, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(RADIO_TX, INPUT_PULLUP);
+
+  // Reset the AD9850
 #ifdef _DEBUG
-  Serial.println("HW Config Done");
+  Serial.println("AD9850 Startup");
 #endif
-  /* Boot up the DDS Module */
   pulseHigh(RESET);
   pulseHigh(W_CLK);
   pulseHigh(FQ_UD);
+  
 #ifdef _DEBUG
-  Serial.println("DDS Config Done");
+  Serial.println("Encoder Startup");
 #endif
+  vfoDial.read();   /* Start up the encoder */
+  lcd.clear();
+  displayFrequency(frequency);
 }
 
 void loop() {
+  long newValue = vfoDial.read();
+  if(newValue != oldValue) {
+    if(oldValue > newValue)
+      frequency -= 5;
+    else
+      frequency += 5;
+    if(frequency > FREQ_MAX)
+      frequency = FREQ_MAX;
+    if(frequency < FREQ_MIN)
+      frequency = FREQ_MIN;
+#ifdef _DEBUG
+    Serial.print("Send AD9850 Command (");
+    Serial.print(frequency);
+    Serial.print("), TX=");  
+    Serial.println(digitalRead(RADIO_TX));
+#endif
+    digitalWrite(LED_BUILTIN, !digitalRead(RADIO_TX));
+    sendFrequency(frequency);
+    displayFrequency(frequency);
+    oldValue = newValue;
+  }
+  displayTx();
+  displayTemp();
 }
-
-/* Serial data transfer to AD9850 */
-void transferByte(byte data) {
+ 
+/*
+ * Serial transfer to the AD9850
+ */
+void transfer_byte(byte data)
+{
   for (int i=0; i<8; i++, data>>=1) {
     digitalWrite(DATA, data & 0x01);
-    pulseHigh(W_CLK);
+    pulseHigh(W_CLK);   //after each bit sent, CLK is pulsed high
   }
 }
 
-/* Set AD9850 operating frequency */
 void sendFrequency(double frequency) {
   if(digitalRead(RADIO_TX) == HIGH)
     frequency += IF_FREQ;
   int32_t freq = frequency * TUNING_VAL/AD9850_FREQ;
   for (int b=0; b<4; b++, freq>>=8) {
-    transferByte(freq & 0xFF);
+    transfer_byte(freq & 0xFF);
   }
-  transferByte(0x000);   // Final control byte, all 0 for 9850 chip
+  transfer_byte(0x000);
   pulseHigh(FQ_UD);
 }
 
-/* We update different bits of the LCD depending on what we're doing */
 void displayFrequency(long freq)
 {
     lcd.setCursor(0, 0);
-    lcd.print("Freq:");
-    lcd.setCursor(6,0);
-    lcd.print(String(freq / 1000));
+    lcd.print("VFO1:");
+    lcd.setCursor(6, 0);
+    lcd.print((float)freq/1000);
+    lcd.setCursor(13, 0);
+    lcd.print("KHz");
+}
+
+void displayTx() {
+    lcd.setCursor(0, 3);
+    if(!digitalRead(RADIO_TX)) {
+      lcd.print("TX");
+    }
+    else {
+      lcd.print("RX");  
+    }
+
+}
+
+void displayTemp() {
+  lcd.setCursor(5, 3);
+  lcd.print("T:");
+  lcd.setCursor(7, 3);
+  lcd.print(temperature);
 }
--- vlf-sdr/vlfsdr.h
+++ vlf-sdr/vlfsdr.h
@@ -14,27 +14,44 @@
 #define _DEBUG    1
 
 /* Hardware Definitions */
+#define TBD_D2    2       /* Digital 2 */
+#define TBD_D3    3       /* Digital 3 */
+#define TBD_D4    4       /* Digital 4 */
+#define TBD_D5    5       /* Digital 5 */
+#define TBD_D6    6       /* Digital 6 */
+#define TBD_D7    7       /* Digital 7 */
 #define W_CLK     8       /* Pin 8 - connect to AD9850 module word load clock pin (CLK) */
 #define FQ_UD     9       /* Pin 9 - connect to freq update pin (FQ) */
 #define DATA      10      /* Pin 10 - connect to serial data load pin (DATA) */
 #define RESET     11      /* Pin 11 - connect to reset pin (RST). */
 #define RADIO_TX  12      /* Pin 12 - transmitter frequency command */
-#define ENC_CW    5
-#define ENC_ACW   6
+#define ENC_CW    6
+#define ENC_ACW   7
+#define TBD_A0    A0      /* Analog 0 */
+#define THERM     A1      /* Pin A1 - Thermistor input for TX temperature */
+#define TBD_A2    A2      /* Analog 2 */
+#define TBD_A3    A3      /* Analog 3 */
 /*
  * Do not use:
  * A4 - (SDA)
  * A5 - (SCL)
  */
+#define TBD_A6    A6      /* Analog 6 */
+#define TBD_A7    A7      /* Analog 7 */
 
 /* Configuration Definitions */
-#define AD9850_FREQ  125000000 /* 125MHz */
-#define TUNING_VAL   4294967295 /* AD9850 Tuning word - used to trim freq out */
-#define IF_FREQ   455000  /* 455KHz */
-#define FREQ_MIN  135700
-#define FREQ_MAX  137800
-#define LCD_ROWS  4
-#define LCD_COLS  20
-#define LCD_ADDR  0x27
- 
+#define AD9850_FREQ   125000000 /* 125MHz */
+#define TUNING_VAL    4294967295 /* AD9850 Tuning word - used to trim freq out */
+#define IF_FREQ       455000  /* 455KHz */
+#define FREQ_MIN      135700
+#define FREQ_MAX      137800
+#define LCD_ROWS      4
+#define LCD_COLS      20
+#define LCD_ADDR      0x3F
+#define THERM_RES     10000
+#define THERM_BETA    3950
+#define THERM_SERIES  9980
+
+#define SW_VERSION "0.1.0"
+
 #define pulseHigh(pin) {digitalWrite(pin, HIGH); digitalWrite(pin, LOW); }
